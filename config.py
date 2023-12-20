da2216dcd967224ab68c82b9a9ee09fba0cf150e
Rinisha1485 <64827843+Rinisha1485@users.noreply.github.com>
2023-12-20 08:38:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cth7x1nv1fnc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "rinisha-employee"
+customregion = "us-east-1"
+
